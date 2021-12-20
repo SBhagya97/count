ea0c08dd75ded9813290b1c75f6412181264fbd4
SBhagya97 <bhagyascbc1997@gmail.com>
2021-12-20 06:34:36 +0530

    vowels

--- src/Vowels.java
+++ src/Vowels.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Vowels {
     public static void main(String[] args) {
         Scanner scn =new Scanner(System.in);
-        System.out.println("enter the String");
+        System.out.print("enter the String:");
         String word=scn.nextLine().toLowerCase();
         char ch[]=word.toCharArray();
         int count=0;
         for (int i:ch) {
             if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u'){
-                System.out.println(word.charAt(i-1)+ " is vowel");
+                System.out.println("letter "+word.indexOf(i)+ " is vowel");
                 count ++;
             }
         }
-        System.out.println("there are "+count +"number of vowels");
+        System.out.println("there are "+count +" number of vowels");
     }
 }
