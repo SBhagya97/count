0d1d7acf559bdd3cc3c42bb3a64666134065a11e
SBhagya97 <bhagyascbc1997@gmail.com>
2021-12-30 15:10:48 +0530

    password

---
+++ src/com/company/Password.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Password {
+    public static void main(String[] args) {
+        List<String> result = new ArrayList<>();
+        int upperCase = 0;
+
+        for (int i = 0; i < str.length; i++) {
+
+            for (int i = 0; i < str.length; i++) {
+                int sum = 0;
+                if (str.isUpperCase(input.charAt(k))) upperCase++;
+
+                if(str.isDigit(input.charAt(k))){
+                    sum = sum + (int) input.charAt(k)
+                }
+
+            }
+            if (upperCase == (int) str[0] && sum == (int) str[4]) {
+                result.add(str);
+            }
+
+
+        }
+
+        String last = str.substring(str.length-4, str.length);
+        last.reverse(last);
+
+        for (int i = 0; i < result.size(); i++ ) {
+            String newStr = result.get(i);
+
+            if(newStr.substring(str.length-4, newStr.length) != last) {
+                result.remove(i);
+            }
+        }
+
+
+        return result.get(0);
+    }
+}
